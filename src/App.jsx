e9d56746b92ebc93576e79ca0dce04e55c9a9d51
deepanshu <gargdeepanshu601@gmail.com>
2024-08-27 17:45:58 +0530

    "new changes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from './Components/Footer/Footer'
 import Navbar from './Components/Home/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import GSCB from './Components/GSCB/GSCB'
 import Home from './Components/Home/Home'
 const App = () => {
+  const [dark,setDark] = useState(false);
+
+  const theme = ()=>{
+    setDark(true);
+    document.body.classList.toggle("dark");}
+
+
+ 
+     
   return (
     <>
-      <Navbar />
+       <Navbar theme={theme} />
 
       <Routes>
         <Route path="/gscb" element={<GSCB />} />
