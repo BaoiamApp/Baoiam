5ab152408d4916fd97b344e2600a555d77005659
abhishek pandit <abhishekpandit.epche@gmail.com>
2024-08-28 23:08:21 +0530

    Reinitializing repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import AboutUs from './pages/AboutUs';
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+const Home = () => (
+  <div className="container mx-auto p-4">
+    <h1 className="text-3xl font-bold mb-4">Home Page</h1>
+    <p className="text-lg text-gray-700">
+      Welcome to the Home page.
+    </p>
+  </div>
+);
+
+export default App;
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,15 @@
+// src/pages/About.jsx
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">About Us</h1>
+      <p className="text-lg text-gray-700">
+        Welcome to the About Us page. Here, you'll find more information about our company, mission, and values.
+      </p>
+    </div>
+  );
+};
+
+export default About;
