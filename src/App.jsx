31d5ff951d74f217a515037135ef77f6898817ec
Linchpin22 <tiwarianuj712u@gmail.com>
2024-08-31 14:02:15 +0530

    Updating brach

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import School from './Pages/School'
 import SkillCatalog from './Components/School/SkillCatalog'
 import CoursePage from './Components/School/CoursePage'
 import SignUp from './Pages/SignUp'
+import Blogs from './Pages/Blogs'
 
 const App = () => {
   const [dark, setDark] = useState(false);
@@ -29,8 +30,8 @@ const App = () => {
         <Route path="/course:id" element={<CoursePage />} />
         <Route path="/course" element={<School />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/blogs" element={<Blogs />} />
       </Routes>
-
       <Footer />
     </>
   )
