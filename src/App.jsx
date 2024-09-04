ed072dc238c3c282adcd45c59fa1ed3993da4667
Rishabh Rander <123rishabhrander@gmail.com>
2024-09-04 19:01:59 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,18 @@ import AboutUs from "./Pages/AboutUs";
 import School from "./Pages/School";
 import PAP from "./Pages/PAP";
 import Checkout from "./Pages/Checkout";
-import ContactUs from './Components/Contact/ContactUs';
-import SkillCatalog from './Components/School/SkillCatalog'
-import CoursePage from './Components/School/CoursePage'
-import SignUp from './Pages/SignUp'
-import College from './Pages/College'
-import Login from './Pages/Login'
-import AboutUs from './Pages/AboutUs'
-import TermsConditions from "./Pages/TermsConditions"
-import PrivacyPolicy from "./Pages/PrivacyPolicy"
-import SchoolCourse from './Pages/SchoolCourse'
-import Blogs from './Pages/Blogs'
-import Career from './Components/Career/Career'
-import Blog_detail from './Pages/Blog_detail'
+import ContactUs from "./Components/Contact/ContactUs";
+import SkillCatalog from "./Components/School/SkillCatalog";
+import CoursePage from "./Components/School/CoursePage";
+import SignUp from "./Pages/SignUp";
+import College from "./Pages/College";
+import Login from "./Pages/Login";
+import TermsConditions from "./Pages/TermsConditions";
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import SchoolCourse from "./Pages/SchoolCourse";
+import Blogs from "./Pages/Blogs";
+import Career from "./Components/Career/Career";
+import Blog_detail from "./Pages/Blog_detail";
 
 const App = () => {
   const [dark, setDark] = useState(false);
@@ -28,13 +27,13 @@ const App = () => {
   const theme = () => {
     setDark(true);
     document.body.classList.toggle("dark");
-  }
+  };
 
   return (
     <div className="dark:bg-[#080529] w-full h-full dark:text-white ">
       <Navbar theme={theme} />
       <Routes>
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/gcsp" element={<GSCB />} />
         <Route path="/courses/school" element={<School />} />
         {/* <Route path="/course/:id" element={<Course />} /> */}
@@ -53,20 +52,18 @@ const App = () => {
 
         <Route path="/login" element={<Login />} />
 
-        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/terms-conditions" element={<TermsConditions />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy/>} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
 
         {/* <Route path="/courses/college/:courseName" element={<College />} /> */}
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/Blog_detail" element={<Blog_detail />} />
         <Route path="/career" element={<Career />} />
-
       </Routes>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
