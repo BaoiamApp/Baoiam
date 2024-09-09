da79560096e365be3e8e8573beaffa1b272ac9e4
αкαѕн gσяαι <akashgorai@Mac.local>
2024-09-09 22:20:13 +0530

    updated ITIE and footer section

--- src/Pages/ITIE.jsx
+++ src/Pages/ITIE.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 import img2 from '../assets/ITIE&Entre/ITIE/img2.jpeg'
-import intern from '../assets/ITIE&Entre/ITIE/intern.jpg'
-import img1 from '../assets/ITIE&Entre/ITIE/img1.jpg'
+import intern1 from '../assets/ITIE&Entre/ITIE/intern1.jpg'
+import productm from '../assets/ITIE&Entre/ITIE/productm .png'
+import hr from '../assets/ITIE&Entre/ITIE/hr.png'
+import dataanalytics from '../assets/ITIE&Entre/ITIE/dataanalytics.png'
+import seo from '../assets/ITIE&Entre/ITIE/seo.png'
+import webd from '../assets/ITIE&Entre/ITIE/webd.png'
+import uiux from '../assets/ITIE&Entre/ITIE/uiux.png'
+import gaphicsdesigning from '../assets/ITIE&Entre/ITIE/gaphicsdesigning.png'
+import businesscomm from '../assets/ITIE&Entre/ITIE/businesscomm.png'
+import marketing from '../assets/ITIE&Entre/ITIE/marketing.png'
+
+
 import { PiGraduationCapFill } from "react-icons/pi";
 import { GoDotFill } from "react-icons/go";
 import { FaPlus } from "react-icons/fa";
@@ -34,55 +44,55 @@ const courses = [
     title: 'Human Resource',
     duration: '3 months',
     description: 'Empowering People, Building Teams with Baoiam.',
-    image: { img1 },
+    image: hr,
   },
   {
     title: 'Data Analytics',
     duration: '3 months',
     description: 'Turn Data in your advantage with BAOIAM',
-    image: { img1 },
+    image: dataanalytics,
   },
   {
     title: 'Marketing',
     duration: '3 months',
     description: 'Make your products/services irresistible.',
-    image: { img1 },
+    image: marketing,
   },
   {
     title: 'SEO',
     duration: '3 months',
     description: 'Attract website traffic and rank higher.',
-    image: { img1 },
+    image: seo,
   },
   {
     title: 'Web Development',
     duration: '3 months',
     description: 'From concept to click, web magic made real.',
-    image: { img1 },
+    image: webd,
   },
   {
     title: 'UI/UX Designing',
     duration: '3 months',
     description: 'Design that sparks curiosity, fuels understanding.',
-    image: { img1 },
+    image: uiux,
   },
   {
     title: 'Gaphics Designing',
     duration: '3 months',
     description: 'Learn the art of visual imagery.',
-    image: { img1 },
+    image: gaphicsdesigning,
   },
   {
     title: 'Product Management',
     duration: '3 months',
     description: 'Building products that users love.',
-    image: { img1 },
+    image: productm,
   },
   {
     title: 'Business Communication',
     duration: '3 months',
     description: 'Mitigate communication barriers.',
-    image: { img1 },
+    image: businesscomm,
   },
 ];
 
@@ -166,7 +176,6 @@ const ITIE = () => {
           <button className="mt-8 bg-gradient-to-r from-indigo-800 to-indigo-500  text-white hover:bg-gradient-to-l transition-all ease-in-out duration-300 py-2 px-6 rounded-full text-lg" onClick={() => navigate('/maintenance')}>
             I AM INTERESTED
 
-
           </button>
         </div>
 
@@ -177,47 +186,30 @@ const ITIE = () => {
       </div>
 
       {/* Program Overview */}
-      <div className="mx-auto p-6 bg-white">
-        <div className="flex flex-col md:flex-row items-center justify-between">
-          <div className="w-full md:w-2/3 mb-4 md:ml-20 md:mb-0">
-            <h1 className="text-3xl md:text-5xl font-bold text-gray-800">
-              Program <span className="text-indigo-600">Overview</span>
-            </h1>
-            <p className="text-gray-600 mb-4  mt-14 text-xl text-justify">
+
+      <div className="md:mx-10">
+        <h1 className="text-3xl md:text-5xl font-bold text-gray-800 text-center mt-10 md:my-10">
+          Program <span className="text-indigo-600">Overview</span>
+        </h1>
+        <div className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
+
+          <div className="w-full md:w-1/2  justify-center md:justify-start md:mr-4 hidden md:block">
+            <img src={intern1} alt="Person" className="rounded-full w-2/3 md:w-4/5 md:ml-20 border-b-8 border-r-8 border-indigo-600 shadow-2xl" />
+          </div>
+
+          <div className="w-full md:w-1/2 p-4">
+            <p className="text-gray-600 mb-4 text-lg md:text-xl text-justify mx-4 md:mx-0 ">
               ITIE is a program that has been formulated by our team and leading experts for young enthusiasts to build a strong and powerful career path. Through this program, not only do the interns get the chance to learn and hone their skills, but also to monetise their skill by cascading the knowledge they have gained so far into others. Hence, after learning a skill, they will be tested on it and if they clear the tests, they get the opportunity to teach a new intern who wishes to learn the same skill as them. The 'Teacher Interns' will then earn exciting compensation offers and incentives.
             </p>
-            <div className="text-red-500 font-medium">
+            <div className="text-red-500 font-medium text-center md:text-left">
               ⏳ Enrollment Deadline Approaching Soon!
-            </div>
-          </div>
-          <div className="w-full  hidden md:block">
-            <div className="relative">
-              <img src={intern} alt="Person" className="rounded-full mx-auto" />
             </div>
           </div>
         </div>
       </div>
 
-      {/* Course Highlight */}
-      {/* <div className="max-w-7xl mx-20 md:mx-auto p-4   mt-16 mb-20 flex-col  ">
-        <h2 className="text-4xl md:text-5xl font-bold text-gray-800 text-center mb-8">
-          Course <span className="text-indigo-600">Highlights</span>
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-14  w-5/6 ml-48 ">
-          {highlights.map((highlight, index) => (
-            <div key={index} className="flex items-center space-x-5 border-b border-gray-900 pb-4">
-              <div className="text-indigo-600 text-4xl size-10">
-                <PiGraduationCapFill />
-              </div>
-              <div>
-                <h3 className="text-xl font-bold text-gray-800">{highlight.title}</h3>
-                <p className="text-gray-600">{highlight.description}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div> */}
 
+      {/* Course Highlight */}
 
       <div className="max-w-7xl mx-4 md:mx-auto p-4 mt-16 mb-20 flex-col">
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-800 text-center mb-8">
@@ -243,9 +235,6 @@ const ITIE = () => {
       </div>
 
 
-
-
-
       {/* Our courses */}
 
       <div className="max-w-6xl mx-auto p-6 bg-white">
@@ -262,9 +251,9 @@ const ITIE = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10 ">
           {courses.map((course, index) => (
-            <div key={index} className="bg-white border-t-4  border border-indigo-600  rounded-t-3xl rounded-b-md hover:shadow-2xl hover:border hover:scale-105 overflow-hidden">
-              <img src={img1} alt={course.title} className="w-full h-48 object-cover" />
-              <div className="p-4">
+            <div key={index} className="border-t-4  border border-indigo-600  rounded-t-3xl rounded-b-md hover:shadow-2xl hover:border hover:scale-105 overflow-hidden">
+              <img src={course.image} alt={course.title} className="w-full h-48 object-cover" />
+              <div className="p-4 h-full bg-indigo-200">
                 <h3 className="text-lg font-bold text-gray-800">{course.title}</h3>
                 <div className="flex items-center justify-between ">
                   <div className="flex items-center text-sm text-indigo-600 font-bold my-2 ">
@@ -285,49 +274,6 @@ const ITIE = () => {
 
 
       {/* Our Testimonials */}
-
-      {/* <div>
-        <h1 className="text-3xl md:text-5xl text-center font-semibold my-8 md:my-14 text-black">
-          Our <span className="text-indigo-600">Testimonials</span>
-        </h1>
-
-        <div className="relative w-full px-4 md:px-10 pb-10 md:pb-20">
-          <Swiper
-            navigation={{
-              prevEl: '.swiper-button-prev',
-              nextEl: '.swiper-button-next',
-            }}
-            modules={[Navigation, Pagination]}
-            spaceBetween={25}
-            slidesPerView={1} 
-            breakpoints={{
-              640: { slidesPerView: 1, spaceBetween: 20 }, 
-              768: { slidesPerView: 2, spaceBetween: 25 }, 
-              1024: { slidesPerView: 3, spaceBetween: 30 }, 
-            }}
-          >
-            {testimonials.map((testimonial, index) => (
-              <SwiperSlide key={index}>
-                <div className="border border-indigo-600 shadow-xl rounded-2xl text-center mt-10 px-6 md:px-10 flex flex-col py-10 w-full h-96">
-                  <img
-                    className="w-16 md:w-24 h-16 md:h-24 mx-auto rounded-full object-cover"
-                    src={testimonial.img}
-                    alt="Student img"
-                  />
-                  <h3 className="text-lg md:text-xl text-gray-800 font-bold mt-4">{testimonial.name}</h3>
-                  <p className="text-gray-600 font-semibold">{testimonial.title}</p>
-                  <p className="mt-2 text-gray-500">{testimonial.content}</p>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-
-
-          <div className="swiper-button-prev bg-indigo-600 rounded-full p-5 md:p-7"></div>
-          <div className="swiper-button-next bg-indigo-600 rounded-full p-5 md:p-7"></div>
-        </div>
-      </div>
- */}
 
 
       <div className="relative">
@@ -365,18 +311,14 @@ const ITIE = () => {
               </SwiperSlide>
             ))}
           </Swiper>
-
-          {/* Navigation buttons */}
           <div className="swiper-button-prev  text-indigo-600 rounded-full p-5 md:p-7 absolute top-1/2 md:left-[-60px] transform -translate-y-1/2 z-10"></div>
           <div className="swiper-button-next  text-indigo-600 rounded-full p-5 md:p-7 absolute top-1/2 md:right-[-60px] transform -translate-y-1/2 z-10"></div>
         </div>
       </div>
 
-
-
       {/* Amazing Career */}
 
-      <div className="flex flex-col items-center justify-center bg-indigo-50 rounded-lg m-3 md:m-10 py-5 md:py-10 px-5 md:px-10">
+      <div className="flex flex-col items-center justify-center bg-indigo-50 rounded-lg m-3 md:m-10 py-5 md:py-14 px-5 md:px-10">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-black text-center">
           Amazing <span className="text-indigo-600">Career</span>
         </h1>
@@ -400,7 +342,7 @@ const ITIE = () => {
             <p className="mt-1 text-base md:text-lg lg:text-xl text-indigo-900">OFF</p>
           </div>
         </div>
-        <p className="mt-6 text-sm md:text-base text-gray-500">Deal Expired!</p>
+
       </div>
 
 
