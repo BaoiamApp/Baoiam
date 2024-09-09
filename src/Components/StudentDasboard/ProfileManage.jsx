9a6c176da2ab0c1132f209d08235ffc46eef69e9
Abhishek Rajput <abhishekrajput9111606916@gmail.com>
2024-09-09 03:36:15 -0700

    fix errors

--- src/Components/StudentDasboard/ProfileManage.jsx
+++ src/Components/StudentDasboard/ProfileManage.jsx
@@ -99,7 +99,6 @@ const ProfileManage = ({ userInfo, setUserInfo }) => {
                 <input
                   type="text"
                   className="py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
-                  className="py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                   placeholder="First Name"
                   onChange={handleInputChange}
                   name="first"
@@ -113,7 +112,6 @@ const ProfileManage = ({ userInfo, setUserInfo }) => {
                 <input
                   type="text"
                   className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
-                  className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                   placeholder="Last Name"
                   onChange={handleInputChange}
                   name="last"
@@ -124,7 +122,6 @@ const ProfileManage = ({ userInfo, setUserInfo }) => {
             <div>
               {!isEditable.personalInfo ? <label className="block py-1 px-2 text-gray-700">{userInfo.mobile || "Enter Mobile Number"}</label> : <input
                 type="tel"
-                className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                 className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                 placeholder="Mobile Number"
                 onChange={handleInputChange}
@@ -138,7 +135,6 @@ const ProfileManage = ({ userInfo, setUserInfo }) => {
                 <label className="block py-1 px-2 text-gray-700">{userInfo.dob}</label> : <input
                   type="date"
                   className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
-                  className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                   name='dob'
                   onChange={handleInputChange}
 
@@ -149,7 +145,6 @@ const ProfileManage = ({ userInfo, setUserInfo }) => {
             <div>
               {!isEditable.personalInfo ? <label className="block py-1 px-2 text-gray-700">{userInfo.college}</label> : <input
                 type="text"
-                className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                 className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                 placeholder="College or School Name"
                 onChange={handleInputChange}
@@ -162,7 +157,6 @@ const ProfileManage = ({ userInfo, setUserInfo }) => {
             <div>
               {!isEditable.personalInfo ? <label className="block py-1 px-2 text-gray-700">{userInfo.location}</label> : <input
                 type="text"
-                className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                 className=" py-1 px-2 block w-full border-gray-300 rounded-md shadow-sm"
                 placeholder="Location"
                 onChange={handleInputChange}
