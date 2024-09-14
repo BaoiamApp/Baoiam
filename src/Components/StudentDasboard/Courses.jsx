618a0a0de3c9d759c48d7736d9ac34c8084bc3c4
ltc01 <laxmichoudhary909@gmail.com>
2024-09-14 15:27:08 +0530

    minor updates

--- src/Components/StudentDasboard/Courses.jsx
+++ src/Components/StudentDasboard/Courses.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchEnrolledCourses } from '../../Redux/slices/courseSlice';
+import { fetchEnrolledCourses } from '../../redux/slices/courseSlice';
 
 
 // const courses = [
