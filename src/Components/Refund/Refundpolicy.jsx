04e5a720a891ddb2abb38b6f99ab8b60af38645e
paulsp01 <paulswarnalee@gmail.com>
2024-09-17 12:23:03 -0400

    commit

--- src/Components/Refund/Refundpolicy.jsx
+++ src/Components/Refund/Refundpolicy.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 const ReturnsPolicy = () => {
   return (
-    <div className="max-w-7xl mx-auto p-4 sm:p-6 md:p-10 bg-white">
+    <div className="max-w-4xl mx-auto p-4 sm:p-6 md:p-10 bg-white">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 flex justify-center items-center pb-4">
         Returns & Refund Policy
       </h1>
-      <p className="lg:text-lg text-sm md:text-base ">
+      <p className="lg:text-base text-sm md:text-base ">
         Thanks for purchasing our products (
         <span className="font-bold">or subscribing to our services</span>) at{" "}
         <span className="font-bold">[WEBSITE]</span> operated by{" "}
         <span className="font-bold">BAOIAM</span>.
       </p>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         We offer a full money-back guarantee on all purchases made on our
         website, but under certain conditions. We invite you to read the
         conditions explained in more detail below, in order to see the condition
         that applies to your situation, while following the correct procedure.
       </p>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         If you are unhappy with the product that you have purchased from us,
         please let us know. Our Returns & Refunds Policy gives you{" "}
         <span className="font-bold">30</span> days to return or exchange an
@@ -29,14 +29,14 @@ const ReturnsPolicy = () => {
         of your purchase.
       </p>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         After the <span className="font-bold">30</span> day period, we cannot
         offer you a refund or exchange. We encourage our customers to try the
         product (or service) in the first <span className="font-bold">30</span>{" "}
         days after their purchase to ensure it meets your needs.
       </p>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         To be eligible for a refund or exchange, all physical products must be
         packed in the original, unmarked packaging including any accessories,
         labels, free gifts, bonus items, manuals, and documentation that shipped
@@ -46,22 +46,22 @@ const ReturnsPolicy = () => {
         original shipping costs invoiced).
       </p>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         If you have any further questions or would like to request a refund,
         please do not hesitate to contact us.
       </p>
       <br />
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold mt-6 mb-2">
+      <h2 className="text-xl sm:text-2xl md:text-2xl font-semibold mt-6 mb-2">
         Refunds (if applicable)
       </h2>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         Once your returned article is received and inspected, we will send you
         an email to notify you that we have received your returned article. We
         will also inform you if your refund has been approved or refused.
       </p>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         If your request is approved, your refund will be processed, and a credit
         will automatically be applied to your credit card or original payment
         method within 14 calendar days. If you have paid for the standard
@@ -69,11 +69,11 @@ const ReturnsPolicy = () => {
         refunded.
       </p>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         In some cases, only partial refunds are granted (if applicable):
       </p>
       <br />
-      <ul className="list-disc list-inside lg:text-lg text-sm md:text-base">
+      <ul className="list-disc list-inside lg:text-base text-sm md:text-base">
         <li>Products with obvious signs of use;</li>
         <br />
         <li>
@@ -84,11 +84,11 @@ const ReturnsPolicy = () => {
         <li>Any article returned more than 30 days after delivery.</li>
       </ul>
       <br />
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold mt-6 mb-2">
+      <h2 className="text-xl sm:text-2xl md:text-2xl font-semibold mt-6 mb-2">
         Late or missing refunds (if applicable)
       </h2>
       <br />
-      <p className="lg:text-lg text-sm md:text-base">
+      <p className="lg:text-base text-sm md:text-base">
         If you haven't received a refund yet, first check your bank account
         again. Then contact your credit card company, it may take some time
         before your refund is officially posted. Next, contact your bank. There
