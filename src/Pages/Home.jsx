bd0222d36d970a483ae7bf9d10cd4b222b68d2af
Aayush-1205 <shindeaayush250@gmail.com>
2024-09-17 17:33:22 +0530

    Added Contact & ITIE

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,25 +11,28 @@ import Slider from '../Components/Home/Slider'
 import Phone from '../Components/Home/Phone'
 import Meetourexperts from '../Components/Home/Meetourexperts'
 import { SuccessLight } from '../assets/assets'
+import ServicesTab from '../Components/AboutUs/ServicesTab'
 
 const Home = () => {
     document.title = 'Baoiam - Home'
     return (
         <>
             <div className='max-w-11/12 mx-auto'>
-            <Hero />
-            <Courses />
-            <Slider />
-            <div className='mx-auto w-full md:w-[85%]'>
-                <img className="w-full h-full" src={SuccessLight} alt="" />
-            </div>
-//             <Categories />
-            <Partners />
-            <Protocol />
-            <JoinNow />
-            <How />
-            <Meetourexperts />
-            <Phone />
+                <Hero />
+                <Courses />
+                <Slider />
+                <div className='mx-auto w-full md:w-[85%] mb-8'>
+                    <img className="w-full h-full" src={SuccessLight} alt="" />
+                </div>
+                {/* Was not looking good, so removed it */}
+                {/* <Categories /> */}
+                <ServicesTab />
+                <Partners />
+                <Protocol />
+                <JoinNow />
+                <How />
+                <Meetourexperts />
+                <Phone />
             </div>
         </>
     )
