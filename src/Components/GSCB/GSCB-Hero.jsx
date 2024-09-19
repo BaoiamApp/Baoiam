62071e06e75815ab155ae6011b6a9dca3adaf587
Abhishek Rajput <abhishekrajput9111606916@gmail.com>
2024-09-19 13:55:47 +0530

    fixe hero section responsiveness

--- src/Components/GSCB/GSCB-Hero.jsx
+++ src/Components/GSCB/GSCB-Hero.jsx
@@ -51,21 +51,6 @@ const HeroSection = () => {
 
         .to(sentence, { display: "none", duration: 2 });
     });
-
-    gsap.fromTo(
-      ".effect",
-      {
-        opacity: 0,
-        y: 30,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 1,
-        ease: "back.inOut",
-        stagger: 0.4,
-      }
-    );
   }, []);
 
   return (
@@ -84,13 +69,13 @@ const HeroSection = () => {
   
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
   
-      <div className="text-white font-extrabold flex flex-col items-center justify-center w-full h-full relative text-center">
-        <div className="sentence gap-4 flex items-center text-center absolute w-[65%] justify-center h-full mx-auto px-10 top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] max-xs:gap-1">
-          <h2 className="bg-red-500 py-3">
+      <div className="text-white font-extrabold flex  flex-col items-start md:items-center justify-center w-full h-full relative text-center">
+        <div className="sentence gap-4 w-full flex flex-col items-start md:flex-row md:items-center text-center absolute md:w-[65%] justify-center h-full mx-auto px-10 top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] max-xs:gap-1">
+          <h2 className="bg-red-500 hero py-3">
             <span className="text-4xl px-4 md:text-4xl lg:text-5xl">GCEP</span>
           </h2>
-          <span className="w-1 h-48 bg-black max-xs:h-24"></span>
-          <p className="text-xl text-left md:text-5xl">Global Collabo <span className="text-nowrap">Educational Partnership</span></p>
+          <span className="w-1 h-36 bg-white hidden md:block max- xs:h-24"></span>
+          <p className="text-xl hero-tail text-left md:text-4xl xl:text-5xl">Global Collabo <span className="text-nowrap">Educational Partnership</span></p>
         
         </div>
       </div>
